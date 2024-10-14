dede21ad815b3ed997820d38b46b909f0332a83e
Pedro Manse <pedro.manse@dmk3.com.br>
2024-10-14 11:23:52 -0300

    fixed some cargo clippy warnings; extracted cmp::min to min3 fn

--- lev.rs
+++ lev.rs
@@ -1,35 +1,30 @@
+fn min3<T: Ord>(a: T, b: T, c: T) -> T {
+    std::cmp::min(std::cmp::min(a, b), c)
+}
 
 // Using the Wagner-Fischer algorithm to compute the Levenshtein distance of two strings
 pub fn lev(s0: &str, s1: &str) -> usize {
-    let s0_chars: Vec<char> = s0.chars().collect();
-    let s1_chars: Vec<char> = s1.chars().collect();    
+    let s1_chars: Vec<char> = s1.chars().collect();
 
-    // get lengths of strings
-    let len_0: usize = s0_chars.len();
+    // get lengths of string 1
     let len_1: usize = s1_chars.len();
 
-    let mut row: Vec<usize> = vec![0 as usize; len_1 + 1];
+    let mut row: Vec<usize> = vec![0; len_1 + 1];
 
-    let mut d0: usize = 0; 
-    let mut e: usize = 0;
+    let mut d0: usize = 0;
+    let mut e;
 
     // initialization
     for i in 0..=len_1 {
         row[i] = i;
     }
 
-    for i in 0..len_0 {
+    for (i, s0_char) in s0.chars().enumerate() {
         e = i + 1;
 
         for j in 0..len_1 {
-            let c: usize = (s0_chars[i] != s1_chars[j]) as usize;
-            d0 = std::cmp::min(
-                    std::cmp::min(
-                        row[j + 1] + 1, 
-                        e + 1
-                    ),
-                    row[j] + c
-                ); 
+            let c: usize = (s0_char != s1_chars[j]) as usize;
+            d0 = min3(row[j + 1] + 1, e + 1, row[j] + c);
 
             row[j] = e;
             e = d0;
@@ -38,9 +33,9 @@ pub fn lev(s0: &str, s1: &str) -> usize {
         row[len_1] = d0;
     }
 
-    return row[len_1];
+    row[len_1]
 }
 
 fn main() {
-  assert_eq!(lev("kitten", "sitting"), 3);
+    assert_eq!(lev("kitten", "sitting"), 3);
 }
